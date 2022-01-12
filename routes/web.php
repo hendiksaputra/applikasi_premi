2cd0c6148c846a0bf82ce2e43e1264de929ae8a6
hendiksaputra <75357856+hendiksaputra@users.noreply.github.com>
2022-01-12 10:31:46 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,23 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('welcome', ['title' => 'Premi Operator Arka']);
 });
+
+Route::get('home', function () {
+    return view('home');
+});
+
+Route::get('projects', [ProjectController::class, 'index']);
+Route::get('projects/add', [ProjectController::class, 'add']);
+Route::post('projects', [ProjectController::class, 'addProcess']);
+Route::get('projects/edit/{id}', [ProjectController::class, 'edit']);
+Route::patch('projects/{id}', [ProjectController::class, 'editProcess']);
+Route::delete('projects/{id}', [ProjectController::class, 'delete']);
+
+Route::get('employees/trash', 'EmployeeController@trash');
+Route::get('employees/restore/{id?}', 'EmployeeController@restore');
+Route::get('employees/delete/{id?}', 'EmployeeController@delete');
+Route::resource('employees', 'EmployeeController');
+
+Route::resource('prod_parameters', 'Prod_parameterController');
