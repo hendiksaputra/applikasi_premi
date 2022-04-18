8cb8ff4e2d68ee00d066db3c94a45daa8127546e
hendiksaputra <75357856+hendiksaputra@users.noreply.github.com>
2022-04-18 15:31:38 +0800

    Update AttendanceCategory.php

--- app/Models/AttendanceCategory.php
+++ app/Models/AttendanceCategory.php
@@ -2,16 +2,12 @@
 
 namespace App\Models;
 
-use App\Models\Attendance;
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
 class AttendanceCategory extends Model
 {
     use HasFactory;
 
-    public function attendance()
-    {
-        return $this->hasMany(Attendance::class);
-    }
+    protected $guarded = [];
 }
