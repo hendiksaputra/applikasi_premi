4a973c1a17c0a423937bbb27b676b4410aacf9e0
hendiksaputra <75357856+hendiksaputra@users.noreply.github.com>
2022-04-18 08:53:11 +0800

    Create index.blade.php

---
+++ resources/views/attendance/index.blade.php
@@ -0,0 +1,83 @@
+@extends('layouts.master')
+
+@section('title')
+    Daftar Attendence
+@endsection
+
+@section('breadcrumb')
+    @parent
+    <li class="active">Attendence</li>
+@endsection
+
+
+@section('content')
+
+<div class="content mt-3">
+    <div class="box">
+        <div class="box-header">
+          <h3 class="box-title">Attendence Data Table</h3>
+          <div class="pull-right">
+            <a href="{{ url('attendances/create') }}" class="btn btn-success btn-sm">
+              <i class="fa fa-plus"></i> Add
+            </a>
+          </div>
+        </div>
+        @if (session('status'))
+        <div class="alert alert-success">
+          {{ session('status') }}
+        </div>
+      @endif
+        <!-- /.box-header -->
+        <div class="box-body">
+          <div id="example2_wrapper" class="dataTables_wrapper form-inline dt-bootstrap"><div class="row">
+              <div class="col-sm-6"></div><div class="col-sm-6"></div>
+          </div>
+          <div class="row"><div class="col-sm-12">
+              <table id="table1" class="table table-bordered table-hover dataTable" role="grid" aria-describedby="example2_info">
+            <thead>
+            <tr role="row">
+                <th class="sorting_asc" tabindex="0" aria-controls="example2" rowspan="1" colspan="1" aria-sort="ascending" aria-label="Rendering engine: activate to sort column descending">No</th>
+                <th class="sorting" tabindex="0" aria-controls="example2" rowspan="1" colspan="1" aria-label="Browser: activate to sort column ascending">NIK</th>
+                <th class="sorting" tabindex="0" aria-controls="example2" rowspan="1" colspan="1" aria-label="Platform(s): activate to sort column ascending">Employee Name</th>
+                <th class="sorting" tabindex="0" aria-controls="example2" rowspan="1" colspan="1" aria-label="Platform(s): activate to sort column ascending">Month/Years</th>
+                <th class="sorting" tabindex="0" aria-controls="example2" rowspan="1" colspan="1" aria-label="Platform(s): activate to sort column ascending">Time</th>
+                <th class="sorting" tabindex="0" aria-controls="example2" rowspan="1" colspan="1" aria-label="Platform(s): activate to sort column ascending">Absen</th>
+                <th class="sorting" tabindex="0" aria-controls="example2" rowspan="1" colspan="1" aria-label="Platform(s): activate to sort column ascending">Action</th>
+            </thead>
+            <tbody>
+                @foreach ($attendances as $item)     
+            <tr role="row" class="odd">
+              <td class="sorting_1">{{ $loop->iteration }}</td>
+              <td>{{ $item->nik }}</td>
+              <td>{{ $item->name_employee }}</td>
+              <td>{{ $item->attendances_month }}</td>
+              <td>{{ $item->present_time }}</td>
+              <td>{{ $item->attendance_value }}</td>
+              <td class="text-center">
+                
+                <form action="{{ url('attendances/' . $item->id) }}" method="post"
+                  onsubmit="return confirm('Are you sure want to delete this data?')" class="d-inline">
+                  @method('delete')
+                  @csrf
+                  <button class="btn btn-danger btn-sm">
+                    <i class="fa fa-trash"></i>
+                  </button>
+                  <a href="{{ url('attendances/' . $item->id . '/edit') }}" class="btn btn-primary btn-sm">
+                    <i class="fa fa-pencil"></i>
+                  </a>
+                </form>
+            
+            
+             </td>
+              
+            </tr>
+            @endforeach
+           </tbody>
+            
+          </table>
+        </div></div><div class="row"><div class="col-sm-5">
+            
+        </div>
+        <!-- /.box-body -->
+      </div>
+@endsection
