43bea876b6ecf5b2d6184825bc563dfd95906a70
hendiksaputra <75357856+hendiksaputra@users.noreply.github.com>
2022-04-18 09:06:37 +0800

    Update edit.blade.php

--- resources/views/employee/edit.blade.php
+++ resources/views/employee/edit.blade.php
@@ -1,86 +1,70 @@
-@extends('main')
+@extends('layouts.master')
 
-@section('title', 'PPO-ARKA')
+@section('title')
+    Form Edit Employee
+@endsection
 
-@section('breadcrumbs')
-  <div class="breadcrumbs">
-    <div class="col-sm-4">
-      <div class="page-header float-left">
-        <div class="page-title">
-          <h1>Employees</h1>
-        </div>
-      </div>
-    </div>
-    <div class="col-sm-8">
-      <div class="page-header float-right">
-        <div class="page-title">
-          <ol class="breadcrumb text-right">
-            <li class="active"><i class="fa fa-users"></i></li>
-          </ol>
-        </div>
-      </div>
-    </div>
-  </div>
+@section('breadcrumb')
+    @parent
+    <li class="active">Edit Employee</li>
 @endsection
 
 @section('content')
-  <div class="content mt-3">
-    <div class="animated fadeIn">
 
-      <div class="card">
-        <div class="card-header">
-          <div class="pull-left">
-            <strong>Edit Data Employee</strong>
-          </div>
-          <div class="pull-right">
-            <a href="{{ url('employees') }}" class="btn btn-success btn-sm">
-              <i class="fa fa-undo"></i> Back
-            </a>
-          </div>
-        </div>
-        <div class="card-body">
-          <div class="row">
-            <div class="col-md-4 offset-md-4">
-              <form action="{{ url('employees/' . $employee->id) }}" method="post">
-                @method('PUT')
-                @csrf
-                <div class="form-group">
-                  <label for="">NIK</label>
-                  <input type="text" name="nik" value="{{ old('nik', $employee->nik) }}"
-                    class="form-control @error('nik') is-invalid @enderror" autofocus>
-                  @error('nik')
-                    <div class="invalid-feedback">{{ $message }}</div>
-                  @enderror
-                </div>
-
-                <div class="form-group">
-                  <label for="">Employee Name</label>
-                  <input type="text" name="name" value="{{ old('name', $employee->name) }}"
-                    class="form-control @error('name') is-invalid @enderror" autofocus>
-                  @error('name')
-                    <div class="invalid-feedback">{{ $message }}</div>
-                  @enderror
-                </div>
-                <div class="form-group">
-                  <label for="">Project Name</label>
-                  <select name="project_id" class="form-control @error('project_id') is-invalid @enderror">
-                    <option value="">- Select -</option>
-                    @foreach ($projects as $item)
-                      <option value="{{ $item->id }}"
-                        {{ old('project_id', $employee->project_id) == $item->id ? 'selected' : null }}>
-                        {{ $item->code }} - {{ $item->name }}</option>
-                    @endforeach
-                  </select>
-                  @error('project_id')
-                    <div class="invalid-feedback">{{ $message }}</div>
-                  @enderror
-                </div>
-                <button type="submit" class="btn btn-success">Save</button>
-              </form>
-            </div>
-          </div>
-        </div>
+<div class="box">
+    <div class="box-header with-border">
+    
+      <div class="pull-right">
+        <a href="{{ url('employees') }}" class="btn btn-warning btn-flat">
+        <i class="fa fa-undo"></i>Back</a>
       </div>
     </div>
+    <!-- /.box-header -->
+<div class="box-body">
+        
+<div class="box box-primary">
+      <div class="box-body">
+        <form action="{{ url('employees/' . $employee->id) }}" method="post">
+            @method('patch')
+            @csrf
+        <div class="form-group  col-md-6">
+          <label for="nik" >NIK</label>
+          <input type="text" class="form-control" name="nik" class="form-control @error('nik') is-invalid @enderror"
+          value="{{ old('nik', $employee->nik) }}" autofocus>
+            @error('nik')
+                <div class="has-warning form-group">{{ $message }}</div>
+            @enderror
+        </div>
+        <div class="form-group  col-md-6">
+          <label for="name_employee" >Employee Name</label>
+          <input type="text" class="form-control" name="name_employee" class="form-control @error('name_employee') is-invalid @enderror"
+          value="{{ old('name_employee', $employee->name_employee) }}" autofocus>
+            @error('name_employee')
+                <div class="has-warning form-group">{{ $message }}</div>
+            @enderror
+        </div>  
+        <div class="form-group  col-md-6">
+            <label for="project_id" >Project</label>
+            <select name="project_id" class="form-control @error('project_id') is-invalid @enderror">
+                <option value="">- Select -</option>
+                @foreach ($projects as $item)
+                  <option value="{{ $item->id }}"
+                    {{ old('project_id', $employee->project_id) == $item->id ? 'selected' : null }}>
+                    {{ $item->code_project }} - {{ $item->name_project }}</option>
+                @endforeach
+              </select>
+              @error('project_id')
+                <div class="invalid-feedback">{{ $message }}</div>
+              @enderror
+          </div>                                                    
+      </div>
+
+      <div class="box-footer">
+        <button type="reset"  class="btn btn-default">Cancel</button>
+        <button type=" submit" class="btn btn-success pull-right">Save</button>
+      </div>
+    </form>
+  
   </div>
+    
 @endsection
