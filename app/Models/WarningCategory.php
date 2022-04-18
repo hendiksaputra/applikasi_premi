228590c3b5b1744f33f40fcfd06f4c3bb00dbc71
hendiksaputra <75357856+hendiksaputra@users.noreply.github.com>
2022-04-18 15:42:20 +0800

    Update WarningCategory.php

--- app/Models/WarningCategory.php
+++ app/Models/WarningCategory.php
@@ -2,16 +2,13 @@
 
 namespace App\Models;
 
-use App\Models\Warning;
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
 class WarningCategory extends Model
 {
     use HasFactory;
 
-    public function warning()
-    {
-        return $this->hasMany(Warning::class);
-    }
+    protected $table ='warning_categories';
+    protected $guarded = [];
 }
