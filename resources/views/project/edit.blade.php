b399a359c759fc727f614e138719312d7fc2742c
hendiksaputra <75357856+hendiksaputra@users.noreply.github.com>
2022-04-18 13:53:42 +0800

    Update edit.blade.php

--- resources/views/project/edit.blade.php
+++ resources/views/project/edit.blade.php
@@ -1,72 +1,56 @@
-@extends('main')
+@extends('layouts.master')
 
-@section('title', 'PPO-ARKA')
+@section('title')
+    Form Edit Project
+@endsection
 
-@section('breadcrumbs')
-  <div class="breadcrumbs">
-    <div class="col-sm-4">
-      <div class="page-header float-left">
-        <div class="page-title">
-          <h1>{{ $title }}</h1>
-        </div>
-      </div>
-    </div>
-    <div class="col-sm-8">
-      <div class="page-header float-right">
-        <div class="page-title">
-          <ol class="breadcrumb text-right">
-            <li class="active"><i class="fa fa-building"></i></li>
-          </ol>
-        </div>
-      </div>
-    </div>
-  </div>
+@section('breadcrumb')
+    @parent
+    <li class="active">Input Edit Project</li>
 @endsection
 
 @section('content')
-  <div class="content mt-3">
-    <div class="animated fadeIn">
 
-      <div class="card">
-        <div class="card-header">
-          <div class="pull-left">
-            <strong>{{ $subtitle }}</strong>
-          </div>
-          <div class="pull-right">
-            <a href="{{ url('projects') }}" class="btn btn-success btn-sm">
-              <i class="fa fa-undo"></i> Back
-            </a>
-          </div>
-        </div>
-        <div class="card-body">
-          <div class="row">
-            <div class="col-md-4 offset-md-4">
-              <form action="{{ url('projects/' . $project->id) }}" method="post">
-                @method('PATCH')
-                @csrf
-                <div class="form-group">
-                  <label for="">Project Code</label>
-                  <input type="text" name="code" class="form-control @error('code') is-invalid @enderror"
-                    value="{{ old('code', $project->code) }}" autofocus>
-                  @error('code')
-                    <div class="text-danger">{{ $message }}</div>
-                  @enderror
-                </div>
-                <div class="form-group">
-                  <label for="">Project Name</label>
-                  <input type="text" name="name" class="form-control @error('name') is-invalid @enderror"
-                    value="{{ old('name', $project->name) }}" autofocus>
-                  @error('name')
-                    <div class="text-danger">{{ $message }}</div>
-                  @enderror
-                </div>
-                <button type="submit" class="btn btn-success">Save
-                </button>
-              </form>
-            </div>
-          </div>
-        </div>
+<div class="box">
+    <div class="box-header with-border">
+    
+      <div class="pull-right">
+        <a href="{{ url('projects') }}" class="btn btn-warning btn-flat">
+        <i class="fa fa-undo"></i>Back</a>
       </div>
     </div>
+    <!-- /.box-header -->
+<div class="box-body">
+        
+<div class="box box-primary">
+      <div class="box-body">
+        <form action="{{ url('projects/' . $projects->id) }}" method="post">
+            @method('patch')
+            @csrf
+        <div class="form-group  col-md-6">
+          <label for="" >Project Code</label>
+          <input type="text" class="form-control" name="code_project" class="form-control @error('code_project') is-invalid @enderror"
+          value="{{ old('code_project', $projects->code_project) }}" autofocus>
+            @error('code_project')
+                <div class="has-warning form-group">{{ $message }}</div>
+            @enderror
+        </div>
+        <div class="form-group  col-md-6">
+          <label for="name_project" >Project Name</label>
+          <input type="text" class="form-control" name="name_project" class="form-control @error('name_project') is-invalid @enderror"
+          value="{{ old('name_project', $projects->name_project) }}" autofocus>
+            @error('name_project')
+                <div class="has-warning form-group">{{ $message }}</div>
+            @enderror
+        </div>                                                   
+      </div>
+
+      <div class="box-footer">
+        <button type="reset"  class="btn btn-default">Cancel</button>
+        <button type=" submit" class="btn btn-success pull-right">Save</button>
+      </div>
+    </form>
+  
   </div>
+    
 @endsection
