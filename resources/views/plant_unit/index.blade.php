14ea60b890988b8245c81ddb58f75916fe9e7f32
hendiksaputra <75357856+hendiksaputra@users.noreply.github.com>
2022-04-18 13:44:44 +0800

    Update index.blade.php

--- resources/views/plant_unit/index.blade.php
+++ resources/views/plant_unit/index.blade.php
@@ -1,126 +1,83 @@
-@extends('main')
+@extends('layouts.master')
 
-@section('title', 'PPO-ARKA')
-
-@section('breadcrumbs')
-  <div class="breadcrumbs">
-    <div class="col-sm-4">
-      <div class="page-header float-left">
-        <div class="page-title">
-          <h1>{{ $title }}</h1>
-        </div>
-      </div>
-    </div>
-    <div class="col-sm-8">
-      <div class="page-header float-right">
-        <div class="page-title">
-          <ol class="breadcrumb text-right">
-            <li class="active"><i class="fa fa-flag"></i></li>
-          </ol>
-        </div>
-      </div>
-    </div>
-  </div>
+@section('title')
+    Daftar Plant Unit
 @endsection
 
+@section('breadcrumb')
+    @parent
+    <li class="active">Plant Unit</li>
+@endsection
+
+
 @section('content')
-  <div class="content mt-3">
-    <div class="animated fadeIn">
-      @if (session('status'))
-        <div class="alert alert-success">
-          {{ session('status') }}
-        </div>
-      @endif
-      <div class="card">
-        <div class="card-header">
-          <div class="pull-left">
-            <strong>{{ $subtitle }}</strong>
-          </div>
-          {{-- @dd($plantUnits) --}}
+
+<div class="content mt-3">
+    <div class="box">
+        <div class="box-header">
+          <h3 class="box-title">Plant Unit Data Table</h3>
           <div class="pull-right">
             <a href="{{ url('plant_units/create') }}" class="btn btn-success btn-sm">
               <i class="fa fa-plus"></i> Add
             </a>
           </div>
         </div>
-        <div class="card-body table-responsive">
-          <table id="plantUnits-table" class="table table-striped table-bordered" width=100%>
-            <thead>
-              <tr>
-                <th width="5%">No</th>
-                <th>Load Category</th>
-                <th>Unit No.</th>
-                <th>Dump Distance</th>
-                <th>Capacity</th>
-                <th width="15%" class="text-center">Action</th>
-              </tr>
-            </thead>
-            {{-- <tbody>
-              @foreach ($plantUnits as $item)
-                <tr>
-                  <td>{{ $loop->iteration }}</td>
-                  <td>{{ $item->load_category->name }}</td>
-                  <td>{{ $item->unit->unit_no }}</td>
-                  <td>{{ $item->dump_distance }}</td>
-                  <td>{{ $item->capacity }}</td>
-                  <td class="text-center">
-                    <a href="{{ url('plant_units/' . $item->id) }}" class="btn btn-warning btn-sm">
-                      <i class="fa fa-eye"></i>
-                    </a>
-                    <a href="{{ url('plant_units/' . $item->id . '/edit') }}" class="btn btn-primary btn-sm">
-                      <i class="fa fa-pencil"></i>
-                    </a>
-                    <form action="{{ url('plant_units/' . $item->id) }}" method="post"
-                      onsubmit="return confirm('Are you sure want to delete this data?')" class="d-inline">
-                      @method('delete')
-                      @csrf
-                      <button class="btn btn-danger btn-sm">
-                        <i class="fa fa-trash"></i>
-                      </button>
-                    </form>
-                  </td>
-                </tr>
-              @endforeach
-            </tbody> --}}
-          </table>
+        @if (session('status'))
+        <div class="alert alert-success">
+          {{ session('status') }}
         </div>
+      @endif
+        <!-- /.box-header -->
+        <div class="box-body">
+          <div id="example2_wrapper" class="dataTables_wrapper form-inline dt-bootstrap"><div class="row">
+              <div class="col-sm-6"></div><div class="col-sm-6"></div>
+          </div>
+          <div class="row"><div class="col-sm-12">
+              <table id="table1" class="table table-bordered table-hover dataTable" role="grid" aria-describedby="example2_info">
+            <thead>
+            <tr role="row">
+                <th class="sorting_asc" tabindex="0" aria-controls="example2" rowspan="1" colspan="1" aria-sort="ascending" aria-label="Rendering engine: activate to sort column descending">No</th>
+                <th class="sorting" tabindex="0" aria-controls="example2" rowspan="1" colspan="1" aria-label="Browser: activate to sort column ascending">Load Category</th>
+                <th class="sorting" tabindex="0" aria-controls="example2" rowspan="1" colspan="1" aria-label="Platform(s): activate to sort column ascending">Unit No</th>
+                <th class="sorting" tabindex="0" aria-controls="example2" rowspan="1" colspan="1" aria-label="Platform(s): activate to sort column ascending">Dump Distance</th>
+                <th class="sorting" tabindex="0" aria-controls="example2" rowspan="1" colspan="1" aria-label="Platform(s): activate to sort column ascending">Capacity</th>
+                
+                <th class="sorting" tabindex="0" aria-controls="example2" rowspan="1" colspan="1" aria-label="Platform(s): activate to sort column ascending">Action</th>
+            </thead>
+            <tbody>
+                @foreach ($plantUnits as $item)   
+            <tr role="row" class="odd">
+              <td class="sorting_1">{{ $loop->iteration }}</td>
+              <td>{{ $item->load_category->name }}</td>
+              <td>{{ $item->unit->unit_no }}</td>
+              <td>{{ $item->dump_distance }}</td>
+              <td>{{ $item->capacity }}</td>
+              
+              <td class="text-center">
+                
+                <form action="{{ url('plant_units/' . $item->id) }}" method="post"
+                  onsubmit="return confirm('Are you sure want to delete this data?')" class="d-inline">
+                  @method('delete')
+                  @csrf
+                  <button class="btn btn-danger btn-sm">
+                    <i class="fa fa-trash"></i>
+                  </button>
+                  <a href="{{ url('plant_units/' . $item->id . '/edit') }}" class="btn btn-primary btn-sm">
+                    <i class="fa fa-pencil"></i>
+                  </a>
+                </form>
+            
+            
+             </td>
+              
+            </tr>
+            @endforeach
+           </tbody>
+            
+          </table>
+        </div></div><div class="row"><div class="col-sm-5">
+            
+        </div>
+        <!-- /.box-body -->
       </div>
-    </div>
-  </div>
-
-  <script src="{{ asset('style/assets/js/lib/data-table/datatables.min.js') }}"></script>
-  <script>
-    $(function() {
-      $("#plantUnits-table").DataTable({
-        processing: true,
-        serverSide: true,
-        ajax: '{{ route('plant_units.index.data') }}',
-        columns: [{
-            data: 'DT_RowIndex',
-            orderable: false,
-            searchable: false
-          },
-          {
-            data: 'load_category_name'
-          },
-          {
-            data: 'unit_no'
-          },
-          {
-            data: 'dump_distance'
-          },
-          {
-            data: 'capacity'
-          },
-          {
-            data: 'action',
-            orderable: false,
-            searchable: false,
-            className: 'text-center'
-          },
-        ],
-        fixedHeader: true,
-      })
-    });
-  </script>
 @endsection
