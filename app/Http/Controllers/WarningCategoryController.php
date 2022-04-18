d7fdcdecd62d2e67c0fa855ee8b9f5caae3c7ef8
hendiksaputra <75357856+hendiksaputra@users.noreply.github.com>
2022-04-18 15:29:20 +0800

    Update WarningCategoryController.php

--- app/Http/Controllers/WarningCategoryController.php
+++ app/Http/Controllers/WarningCategoryController.php
@@ -2,25 +2,49 @@
 
 namespace App\Http\Controllers;
 
+use app\Model\WarningCategory;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
 class WarningCategoryController extends Controller
 {
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
     public function index()
     {
         $title = 'Warning Categories';
-        $subtitle = 'Warning Categories Data';
         $warningCategories = DB::table('warning_categories')->get();
-        return view('warning_category.index', compact('title','subtitle','warningCategories'));
+        return view('warning_category.index', compact('title','warningCategories'));
     }
 
     public function add()
     {
         $title = 'Warning Categories';
-        $subtitle = 'Add Warning Categories';
-        return view('warning_category.add', compact('title','subtitle'));
+        return view('warning_category.add', compact('title'));
     }
+
+    
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    
 
     public function addProcess(Request $request)
     {
@@ -35,13 +59,38 @@ class WarningCategoryController extends Controller
         return redirect('warning_categories')->with('status', 'Category added successfully');
     }
 
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
     public function edit($id)
     {
-        $title = 'Warning Categories';
-        $subtitle = 'Edit Warning Categories';
         $warningCategories = DB::table('warning_categories')->where('id', $id)->first();
-        return view('warning_category.edit', compact('title','subtitle','warningCategories'));
+        return view('warning_category.edit', compact('warningCategories'));
     }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     * 
+     * 
+     */
 
     public function editProcess(Request $request, $id)
     {
@@ -55,6 +104,21 @@ class WarningCategoryController extends Controller
             'warning_index' => $request->warning_index
         ]);
         return redirect('warning_categories')->with('status', 'Category updated successfully');
+    }
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
     }
 
     public function delete($id)
