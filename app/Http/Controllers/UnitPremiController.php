d69659bbc871d29299e238e73493c4f1029dd878
hendiksaputra <75357856+hendiksaputra@users.noreply.github.com>
2022-04-18 15:28:36 +0800

    Update UnitPremiController.php

--- app/Http/Controllers/UnitPremiController.php
+++ app/Http/Controllers/UnitPremiController.php
@@ -2,8 +2,8 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Unit;
 use App\Models\UnitPremi;
+use App\Models\Unit;
 use Illuminate\Http\Request;
 
 class UnitPremiController extends Controller
@@ -15,11 +15,10 @@ class UnitPremiController extends Controller
      */
     public function index()
     {
-        $title = 'Unit Premis';
-        $subtitle = 'Unit Premis Data';
         $unitPremis = UnitPremi::with('unit')->latest()->get();
 
-        return view('unit_premi.index', compact('title', 'subtitle', 'unitPremis'));
+        return view('unit_premi.index', compact( 'unitPremis'));
+    
     }
 
     /**
@@ -29,10 +28,9 @@ class UnitPremiController extends Controller
      */
     public function create()
     {
-        $title = 'Unit Premis';
-        $subtitle = 'Add Unit Premi';
         $unit = Unit::with('unit_model','project')->orderBy('unit_no','asc')->get();
-        return view('unit_premi.add', compact('title','subtitle','unit'));
+        return view('unit_premi.add', compact('unit'));
+    
     }
 
     /**
@@ -55,6 +53,7 @@ class UnitPremiController extends Controller
         UnitPremi::create($request->all()); 
 
         return redirect('unit_premis')->with('status', 'Unit Premi added successfully');
+    
     }
 
     /**
@@ -65,10 +64,7 @@ class UnitPremiController extends Controller
      */
     public function show(UnitPremi $unitPremi)
     {
-        $title = 'Unit Premis';
-        $subtitle = 'Unit Premi Detail';
-        // $unitPremi = UnitPremi::with('unit')->get();
-        return view('unit_premi.show', compact('title','subtitle','unitPremi'));
+        //
     }
 
     /**
@@ -79,10 +75,10 @@ class UnitPremiController extends Controller
      */
     public function edit(UnitPremi $unitPremi)
     {
-        $title = 'Unit Premis';
-        $subtitle = 'Edit Unit Premi';
+       
         $unit = Unit::with('unit_model','project')->orderBy('unit_no','asc')->get();
-        return view('unit_premi.edit', compact('title','subtitle','unit','unitPremi'));
+        return view('unit_premi.edit', compact('unit','unitPremi'));
+    
     }
 
     /**
@@ -109,6 +105,7 @@ class UnitPremiController extends Controller
         ]);
 
         return redirect('unit_premis')->with('status', 'Unit Premi updated successfully');
+    
     }
 
     /**
@@ -122,25 +119,6 @@ class UnitPremiController extends Controller
         $unitPremi->delete();
 
         return redirect('unit_premis')->with('status', 'Unit Premi deleted successfully');
-    }
-
-    public function index_data()
-    {
-        $unitPremis = UnitPremi::latest()->get();
-
-        return datatables()->of($unitPremis)
-            ->addIndexColumn()
-            ->addColumn('unit_no', function($unitPremis){
-                return $unitPremis->unit->unit_no;
-            })
-            ->addColumn('code', function($unitPremis){
-                return $unitPremis->unit->project->code;
-            })
-            ->addColumn('premi', function($unitPremis){
-                return $unitPremis->premi;
-            })
-            ->addColumn('action', 'unit_premi.action')
-            ->rawColumns(['action'])
-            ->toJson();
+    
     }
 }
