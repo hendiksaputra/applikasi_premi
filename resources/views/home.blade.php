f9b67cf259dd6ce20ca0316aef8369da4b342938
hendiksaputra <75357856+hendiksaputra@users.noreply.github.com>
2022-04-18 14:26:52 +0800

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,32 +1,15 @@
-@extends('main')
+@extends('layouts.master')
 
-@section('title', 'Dashboard')
-
-@section('breadcrumbs')
-  <div class="breadcrumbs">
-    <div class="col-sm-4">
-      <div class="page-header float-left">
-        <div class="page-title">
-          <h1>Dashboard</h1>
-        </div>
-      </div>
-    </div>
-    <div class="col-sm-8">
-      <div class="page-header float-right">
-        <div class="page-title">
-          <ol class="breadcrumb text-right">
-            <li class="active"><i class="fa fa-dashboard"></i></li>
-          </ol>
-        </div>
-      </div>
-    </div>
-  </div>
+@section('title')
+    Dashboard
 @endsection
 
+@section('breadcrumb')
+    @parent
+    <li class="active">Dashboard</li>
+@endsection
+
+
 @section('content')
-  <div class="content mt-3">
-    <div class="animated fadeIn">
-      Welcome back, <strong>{{ auth()->user()->name }}</strong>
-    </div>
-  </div>
+    
 @endsection
