e13a5a75e65fdd2737e7df9d1b5cd56cc852ccc0
hendiksaputra <75357856+hendiksaputra@users.noreply.github.com>
2022-04-18 14:17:16 +0800

    Update add.blade.php

--- resources/views/unit_premi/add.blade.php
+++ resources/views/unit_premi/add.blade.php
@@ -1,75 +1,60 @@
-@extends('main')
+@extends('layouts.master')
 
-@section('title', 'PPO-ARKA')
+@section('title')
+    Form Input Unit Premi
+@endsection
 
-@section('breadcrumbs')
-  <div class="breadcrumbs">
-    <div class="col-sm-4">
-      <div class="page-header float-left">
-        <div class="page-title">
-          <h1>{{ $title }}</h1>
-        </div>
-      </div>
-    </div>
-    <div class="col-sm-8">
-      <div class="page-header float-right">
-        <div class="page-title">
-          <ol class="breadcrumb text-right">
-            <li class="active"><i class="fa fa-rub"></i></li>
-          </ol>
-        </div>
-      </div>
-    </div>
-  </div>
+@section('breadcrumb')
+    @parent
+    <li class="active">Input Data Unit Premi</li>
 @endsection
 
 @section('content')
-  <div class="content mt-3">
-    <div class="animated fadeIn">
-      <div class="card">
-        <div class="card-header">
-          <div class="pull-left">
-            <strong>{{ $subtitle }}</strong>
-          </div>
-          <div class="pull-right">
-            <a href="{{ url('unit_premis') }}" class="btn btn-success btn-sm">
-              <i class="fa fa-undo"></i> Back
-            </a>
-          </div>
-        </div>
-        <div class="card-body">
-          <div class="row">
-            <div class="col-md-8">
-              <form action="{{ url('unit_premis') }}" method="post">
-                @csrf
-                <div class="form-group">
-                  <label for="">Unit No</label>
-                  <select data-placeholder="Choose an unit..."
-                    class="standardSelect @error('unit_id') is-invalid @enderror" tabindex="1" name="unit_id" autofocus>
-                    <option value=""></option>
-                    @foreach ($unit as $item)
-                      <option value="{{ $item->id }}" {{ old('unit_id') == $item->id ? 'selected' : null }}>
-                        {{ $item->unit_no }} - {{ $item->unit_desc }} - {{ $item->project->code }}</option>
-                    @endforeach
-                  </select>
-                  @error('unit_id')
-                    <div class="text-danger">{{ $message }}</div>
-                  @enderror
-                </div>
-                <div class="form-group">
-                  <label for="">Premi</label>
-                  <input type="text" name="premi" class="form-control @error('premi') is-invalid @enderror"
-                    value="{{ old('premi') }}" autofocus>
-                  @error('premi')
-                    <div class="text-danger">{{ $message }}</div>
-                  @enderror
-                </div>
-                <button type=" submit" class="btn btn-success">Save</button>
-              </form>
-            </div>
-          </div>
-        </div>
+
+<div class="box">
+    <div class="box-header with-border">
+    
+      <div class="pull-right">
+        <a href="{{ url('unit_premis') }}" class="btn btn-warning btn-flat">
+        <i class="fa fa-undo"></i>Back</a>
       </div>
     </div>
+    <!-- /.box-header -->
+<div class="box-body">
+        
+<div class="box box-primary">
+      <div class="box-body">
+        <form action="{{ url('unit_premis') }}" method="post">
+            @csrf
+        <div class="form-group  col-md-6">
+          <label for="unit_id" >Unit No *</label>
+          <select name="unit_id" class="form-control @error('unit_id') is-invalid @enderror">
+            <option value="">- Select -</option>
+            @foreach ($unit as $item)
+                      <option value="{{ $item->id }}" {{ old('unit_id') == $item->id ? 'selected' : null }}>
+                        {{ $item->unit_no }} - {{ $item->unit_desc }} - {{ $item->project->code_project }}</option>
+            @endforeach
+          </select>
+          @error('unit_id')
+            <div class="invalid-feedback">{{ $message }}</div>
+          @enderror
+        </div>
+        <div class="form-group  col-md-6">
+          <label for="premi" >Premi *</label>
+          <input type="text" class="form-control" name="premi"  class="form-control @error('premi') is-invalid @enderror"
+          value="{{ old('premi') }}" autofocus>
+            @error('premi')
+                <div class="has-warning form-group">{{ $message }}</div>
+            @enderror
+        </div>                                                   
+      </div>
+
+      <div class="box-footer">
+        <button type="reset"  class="btn btn-default">Cancel</button>
+        <button type=" submit" class="btn btn-success pull-right">Save</button>
+      </div>
+    </form>
+  
   </div>
+    
 @endsection
