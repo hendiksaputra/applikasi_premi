c14c059c3c1946f54459bdfb8161340493aa015e
hendiksaputra <75357856+hendiksaputra@users.noreply.github.com>
2022-04-18 15:17:59 +0800

    Update AttendanceController.php

--- app/Http/Controllers/AttendanceController.php
+++ app/Http/Controllers/AttendanceController.php
@@ -6,7 +6,6 @@ use App\Models\Attendance;
 use App\Models\AttendanceCategory;
 use App\Models\Employee;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
 
 class AttendanceController extends Controller
 {
@@ -15,81 +14,12 @@ class AttendanceController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    // public function index()
-    // {
-    //     $title = 'Attendances';
-    //     $subtitle = 'Attendances Data';
-    //     $attendances = Attendance::with('employee','attendance_category')->latest()->get();
-    
-    //     return view('attendance.index', compact('title', 'subtitle', 'attendances','category'));
-    // }
-    
-    public function index(Request $request)
+    public function index()
     {
-        $title = 'Attendances';
-        $subtitle = 'Attendances Data';
-        $category = AttendanceCategory::orderBy('code','asc')->get();
-        if ($request->ajax()) {
-            $attendances = DB::table('attendances')
-                        ->select(['attendances.id','employees.nik','employees.name','attendances.present_time','attendance_categories.code','attendance_categories.remarks'])
-                        ->join('employees','attendances.employee_id','=','employees.id')
-                        ->join('attendance_categories','attendances.attendance_category_id','=','attendance_categories.id')
-                        ->orderBy('attendances.id', 'desc');
-            return datatables()->of($attendances)
-                ->addIndexColumn()
-                ->addColumn('nik', function($attendances){
-                    return $attendances->nik;
-                })
-                ->addColumn('name', function($attendances){
-                    return $attendances->name;
-                })
-                ->addColumn('time', function($attendances){
-                    return $attendances->present_time;
-                })
-                ->addColumn('category', function($attendances){
-                    return $attendances->code .' - '.$attendances->remarks;
-                })
-                ->filter(function ($instance) use ($request) {
-                    if (!empty($request->get('date1') && !empty($request->get('date2')))) {
-                        $instance->where(function($w) use($request){
-                            $date1 = $request->get('date1');
-                            $date2 = $request->get('date2');
-                            $w->whereBetween(DB::raw('DATE(present_time)'), array($date1, $date2));
-                        });
-                    }
-                    if (!empty($request->get('nik'))) {
-                        $instance->where(function($w) use($request){
-                            $nik = $request->get('nik');
-                            $w->orWhere('nik', 'LIKE','%'.$nik.'%');
-                        });
-                    }
-                    if (!empty($request->get('name'))) {
-                        $instance->where(function($w) use($request){
-                            $name = $request->get('name');
-                            $w->orWhere('name', 'LIKE','%'.$name.'%');
-                        });
-                    }
-                    if (!empty($request->get('category'))) {
-                        $instance->where(function($w) use($request){
-                            $category = $request->get('category');
-                            $w->orWhere('code', 'LIKE','%'.$category.'%');
-                        });
-                    }
-                    if (!empty($request->get('search'))) {
-                            $instance->where(function($w) use($request){
-                            $search = $request->get('search');
-                            $w->orWhere('name', 'LIKE', "%$search%")
-                            ->orWhere('nik', 'LIKE', "%$search%");
-                        });
-                    }
-                }, true)
-                ->addColumn('action', 'attendance.action')
-                ->rawColumns(['action'])
-                ->toJson();
-        } else {
-            
-        }
-        return view('attendance.index', compact('title','subtitle','category'));
+        
+        $attendances = Attendance::with('employee','attendance_category')->latest()->get();
+
+        return view('attendance.index', compact('attendances'));
     }
 
     /**
@@ -99,12 +29,10 @@ class AttendanceController extends Controller
      */
     public function create()
     {
-        $title = 'Attendances';
-        $subtitle = 'Add Attendances';
         $employees = Employee::with('project')->orderBy('nik','asc')->get();
         $category = AttendanceCategory::orderBy('code','asc')->get();
 
-        return view('attendance.add', compact('title', 'subtitle', 'employees','category'));
+        return view('attendance.add', compact('employees','category'));
     }
 
     /**
@@ -116,18 +44,24 @@ class AttendanceController extends Controller
     public function store(Request $request)
     {
         $request->validate([
-            'employee_id' => 'required',
+            'nik' => 'required',
+            'name_employee' => 'required',
+            'attendances_month' => 'required',
             'present_time' => 'required',
-            'attendance_category_id' => 'required'
+            'attendance_value' => 'required'
         ],[
-            'employee_id.required' => 'Employee is required',
-            'present_time.required' => 'Present Time is required',
-            'attendance_category_id.required' => 'Attendance Category is required',
+            'nik.required' => 'NIK is required',
+            'name_employee.required' => 'Name Employee Month is required',
+            'attendances_month.required' => 'Attendances Time is required',
+            'present_time.required' => 'Present Time  is required',
+            'attendance_value.required' => 'Attendance Value is required',
         ]);
         Attendance::create([
-            'employee_id' => $request->employee_id,
+            'nik' => $request->nik,
+            'name_employee' => $request->name_employee,
+            'attendances_month' => $request->attendances_month,
             'present_time' => $request->present_time,
-            'attendance_category_id' => $request->attendance_category_id
+            'attendance_value' => $request->attendance_value
 
         ]);
         return redirect('attendances')->with('status', 'Attendance added successfully');
@@ -141,11 +75,7 @@ class AttendanceController extends Controller
      */
     public function show(Attendance $attendance)
     {
-        $title = 'Attendances';
-        $subtitle = 'Attendances Detail';
-        // $attendances = Attendance::with('employee','attendance_category')->latest()->get();
-
-        return view('attendance.show', compact('title', 'subtitle', 'attendance'));
+        //
     }
 
     /**
@@ -156,12 +86,10 @@ class AttendanceController extends Controller
      */
     public function edit(Attendance $attendance)
     {
-        $title = 'Attendances';
-        $subtitle = 'Edit Attendances';
         $employees = Employee::with('project')->orderBy('nik','asc')->get();
         $category = AttendanceCategory::orderBy('code','asc')->get();
 
-        return view('attendance.edit', compact('title', 'subtitle', 'employees','category','attendance'));
+        return view('attendance.edit', compact('employees','category','attendance'));
     }
 
     /**
@@ -174,18 +102,25 @@ class AttendanceController extends Controller
     public function update(Request $request, Attendance $attendance)
     {
         $request->validate([
-            'employee_id' => 'required',
+            'nik' => 'required',
+            'name_employee' => 'required',
+            'attendances_month' => 'required',
             'present_time' => 'required',
-            'attendance_category_id' => 'required'
+            'attendance_value' => 'required'
         ],[
-            'employee_id.required' => 'Employee is required',
-            'present_time.required' => 'Present Time is required',
-            'attendance_category_id.required' => 'Attendance Category is required',
+            'nik.required' => 'NIK is required',
+            'name_employee.required' => 'Name Employee Month is required',
+            'attendances_month.required' => 'Attendances Time is required',
+            'present_time.required' => 'Present Time  is required',
+            'attendance_value.required' => 'Attendance Value is required',
         ]);
         Attendance::where('id', $attendance->id)->update([
-            'employee_id' => $request->employee_id,
+            'nik' => $request->nik,
+            'name_employee' => $request->name_employee,
+            'attendances_month' => $request->attendances_month,
             'present_time' => $request->present_time,
-            'attendance_category_id' => $request->attendance_category_id
+            'attendance_value' => $request->attendance_value
+
 
         ]);
         return redirect('attendances')->with('status', 'Attendance edited successfully');
@@ -201,65 +136,5 @@ class AttendanceController extends Controller
     {
         $attendance->delete();
         return redirect('attendances')->with('status', 'Attendance deleted successfully');
-    }
-
-    public function index_data()
-    {
-        $attendances = Attendance::latest()->get();
-
-        return datatables()->of($attendances)
-            ->addIndexColumn()
-            ->addColumn('nik', function($attendances){
-                return $attendances->employee->nik;
-            })
-            ->addColumn('name', function($attendances){
-                return $attendances->employee->name;
-            })
-            ->addColumn('time', function($attendances){
-                return $attendances->present_time;
-            })
-            ->addColumn('category', function($attendances){
-                return $attendances->attendance_category->code .' - '.$attendances->attendance_category->remarks;
-            })
-            ->addColumn('action', 'attendance.action')
-            ->rawColumns(['action'])
-            ->toJson();
-    }
-
-    public function trash()
-    {
-        $title = 'Attendances';
-        $subtitle = 'Attendances Data - Deleted';
-        $attendances = Attendance::onlyTrashed()->latest()->get();
-        return view('attendance.trash', compact('title','subtitle','attendances'));
-
-    }
-
-    public function restore ($id = null)
-    {
-        if($id != null){
-            $attendances = Attendance::onlyTrashed()
-            ->where('id', $id)
-            ->restore();
-        } else {
-            $attendances = Attendance::onlyTrashed()
-            ->restore();
-        }
-
-        return redirect('attendances/trash')->with('status', 'Attendance restored successfully');
-    }
-
-    public function delete($id = null)
-    {
-        if($id != null){
-            $attendances = Attendance::onlyTrashed()
-            ->where('id', $id)
-            ->forceDelete();
-        } else {
-            $attendances = Attendance::onlyTrashed()
-            ->forceDelete();
-        }
-
-        return redirect('attendances/trash')->with('status', 'Attendance deleted permanent successfully');
     }
 }
